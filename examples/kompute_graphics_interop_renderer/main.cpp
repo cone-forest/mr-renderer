81d6a1b6ee1eed5f14e90f3dedb9faa35c34b2b8
Michael Tsukanov <tsukanovWOW@ya.ru>
2026-04-29 01:37:10 +0300

    Add executable for the interop example

---
+++ examples/kompute_graphics_interop_renderer/main.cpp
@@ -0,0 +1,20 @@
+#include <utility>
+
+#include "kompute_graphics_interop_renderer.hpp"
+#include <mr-renderer/file_presenter.hpp>
+
+int main()
+{
+  mr::KomputeGraphicsInteropRenderer renderer{256, 256};
+  mr::FilePresenter presenter{"frames_out"};
+
+  int frames_number = 3;
+  for (auto&& frame : renderer.frames()) {
+    presenter.present(std::move(frame));
+    if (frames_number-- == 0) {
+      break;
+    }
+  }
+
+  return 0;
+}
